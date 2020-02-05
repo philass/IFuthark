0bbcdafb07e3a55091ab43631e2df3c55321e01d
Philip Lassen <philiplassen@gmail.com>
2020-02-05 02:56:04 +0100

    Fixed excess character issue (removed REPL line numbers); still broken on empty lines

--- fut_kernel.py
+++ fut_kernel.py
@@ -2,8 +2,11 @@
 Futhark Wrapper Kernel
 """
 
+import re
+
 from ipykernel.kernelbase import Kernel
 from pexpect.replwrap import REPLWrapper
+
 class FutharkKernel(Kernel):
   """
   Implementation of the Method and Attributes
@@ -32,7 +35,10 @@ class FutharkKernel(Kernel):
   def do_execute(self, code, silent, store_history=True, user_expressions=None,
                  allow_stdin=False):
     if not silent:
+
       output = self.repl.run_command(code)
+      output = re.sub(r'\r\n\[\d+', '\n', output)
+      output = re.sub(r'\r\[\d+', '\n', output)
       stream_content = {'name': 'stdout', 'text': output}
       self.send_response(self.iopub_socket, 'stream', stream_content)
 
